2f7c2aff20d467951333bcdfce79e594421ed360
morten <mmat@eaaa.dk>
2014-09-21 13:03:29 +0200

    Fix copyright year

--- www/js/remoteDatabase.js
+++ www/js/remoteDatabase.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  * 
- * Copyright (c) <2004> <Business Academy Aarhus>
+ * Copyright (c) <2014> <Business Academy Aarhus>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
